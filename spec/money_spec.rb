22f13691defeaf8d7f69c25e787867d38832de89
mckyhrs <af.18.715@gmail.com>
2021-12-24 23:03:52 +0900

    finished section1/chapter5

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,13 +1,19 @@
 require_relative '../lib/Dollar'
+require_relative '../lib/Franc'
 
 RSpec.describe do
-  it 'test_multiplication' do
+  it 'test multiplication' do
     five = Dollar.new(5)
     expect(Dollar.new(10).equals(five.times(2))).to eq true
     expect(Dollar.new(15).equals(five.times(3))).to eq true
   end
-  it 'test_equality' do
+  it 'test equality' do
     expect(Dollar.new(5).equals(Dollar.new(5))).to eq true
     expect(Dollar.new(5).equals(Dollar.new(6))).to eq false
+  end
+  it 'test franc multiplication' do
+    five = Franc.new(5)
+    expect(Franc.new(10).equals(five.times(2))).to eq true
+    expect(Franc.new(15).equals(five.times(3))).to eq true
   end
 end
