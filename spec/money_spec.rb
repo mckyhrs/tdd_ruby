7190716688b06e2a15efd2f48ef9d53f392447ff
mckyhrs <af.18.715@gmail.com>
2021-12-24 22:30:38 +0900

    finished section1/chapter3

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,11 +1,15 @@
-require_relative '../../../lib/section1/chapter1/Dollar'
+require_relative '../lib/Dollar'
 
 RSpec.describe do
-  it '' do
+  it do
     five = Dollar.new(5)
     product = five.times(2)
     expect(product.amount).to eq 10
     product = five.times(3)
     expect(product.amount).to eq 15
+  end
+  it do
+    expect(Dollar.new(5).equals(Dollar.new(5))).to eq true
+    expect(Dollar.new(5).equals(Dollar.new(6))).to eq false
   end
 end
